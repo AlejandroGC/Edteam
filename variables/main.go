fa3203391298d303604e44793c7b243a8ecda571
Alejandro Gonzalez <alejandro.g.carrillo@hotmail.com>
2020-10-26 03:29:13 -0600

    Learning Go variable and constants

---
+++ variables/main.go
@@ -0,0 +1,21 @@
+// package main es el paquete principal que me permite ejecutar mi aplicacion de EDteam
+package main
+
+// biblioteca que nos permite tener las bases y los instrumentos principales de Go
+import "fmt"
+
+func main() {
+	// VARIABLES
+	dog, cat := "perro", "gato"
+	cat, face := "gatito", "cara"
+	fmt.Println(dog, cat, face)
+
+
+	// CONSTANTES
+	const pi = 3.14
+	fmt.Println(pi)
+
+	// COMENTARIOS
+	// /* comentario multilinea */
+
+}
