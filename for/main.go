1484de6930c9effc3afe889095f168e080ae3489
Alejandro Gonzalez <alejandro.g.carrillo@hotmail.com>
2020-10-29 02:09:31 -0600

    if, for, while finished

---
+++ for/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main(){
+	// For clasico
+	// for i := 1; i<=10; i++{
+	// 	fmt.Println(i)
+	// }
+
+	// For continuo
+	// i:=1
+	// for i <= 10{
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// for forever
+	// i = 0
+	// for {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// For range slices
+	// nums := []uint8{2,4,6,8}
+
+	// for _, v := range nums {
+	// 	v *= 2
+	// }
+	// fmt.Println(nums)
+
+	// for i := range nums {
+	// 	nums[i] *= 2
+	// }
+	// fmt.Println(nums)
+
+
+}
